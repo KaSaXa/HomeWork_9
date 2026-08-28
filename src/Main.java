fcdcc7117d11f85d264b09ea2ee3a80036369e8d
KaSaXa <akurmelevv@gmail.com>
2026-08-28 15:17:31 +0300

    1.2

--- src/Main.java
+++ src/Main.java
@@ -45,48 +45,24 @@ public class Main {
 
 //task 2
         int[] inputArray2 = {32000, 24000, 18000, 26000, 50000};
+        int[] outputArray2 = new int[inputArray2.length];
 
-        int[] outputArray2 = new int[4];
-        int sum1 = 0;
-        int max1 = inputArray2[0];
-        int min1 = inputArray2[0];
+        int index = 0;
 
-// вместо ручного отсчёта я использовал попроще метод с других сайтов. так же можно?
-        int[] percentArray2 = new int[inputArray2.length];
-        for (int i = 0; i < inputArray2.length; i++) {
-            percentArray2[i] = (int) Math.round(inputArray2[i] * 0.13);
+        for (int value : inputArray2) {
+            outputArray2[index] = (int) Math.round(value * 0.13);
+            index++;
         }
-
-        for (int value1 : inputArray2) {
-            sum1 += value1;
-            if (value1 > max1) {
-                max1 = value1;
-            }
-            if (value1 < min1) {
-                min1 = value1;
-            }
-        }
-        int average1 = (int) sum1 / inputArray2.length;
-        outputArray2[0] = sum1;
-        outputArray2[1] = max1;
-        outputArray2[2] = min1;
-        outputArray2[3] = average1;
 
         System.out.println("Входной массив (выплаты сотрудникам):");
-        for (int value1 : inputArray2) {
-            System.out.print(value1 + " ");
-        }
-        System.out.println();
-// Я не понимаю, как ввести 13% в выходной массив совмещённо(если надо было)
-        System.out.println("13% от каждой выплаты:");
-        for (int value1 : percentArray2) {
-            System.out.print(value1 + " ");
+        for (int value : inputArray2) {
+            System.out.print(value + " ");
         }
         System.out.println();
 
-        System.out.println("Выходной массив [сумма, максимум, минимум, среднее]:");
-        for (int value1 : outputArray2) {
-            System.out.print(value1 + " ");
+        System.out.println("13% от каждой выплаты:");
+        for (int value : outputArray2) {
+            System.out.print(value + " ");
         }
         System.out.println();
 
@@ -95,29 +71,30 @@ public class Main {
         int[] inputArray3 = {2000, 6000, 8000, 1000, 3000};
         boolean[] outputArray3 = new boolean[inputArray3.length];
 
-        for (int i = 0; i < inputArray3.length; i++) {
-            outputArray3[i] = inputArray3[i] > 5000;
+        int index1 = 0;
+        for (int number : inputArray3) {
+            outputArray3[index1] = number > 5000;
+            index1++;
         }
 
-// Так же можно? В голове всё перемешалось и я очень начинаю путаться.
-///Взял с других ресурсов
         System.out.println("inputArray3: " + java.util.Arrays.toString(inputArray3));
         System.out.println("outputArray3: " + java.util.Arrays.toString(outputArray3));
-
 //task 4
 
-                int[] inputArray4 = {3260, 2000, -5760, 300, 1000};
-                boolean outputArray4 = true;
+        int[] inputArray4 = {3260, 2000, -5760, 300, 1000};
+        boolean[] outputArray4 = new boolean[1];
+        outputArray4[0] = true;
 
-                for (int balance : inputArray4) {
-                    if (balance < 0) {
-                        outputArray4 = false;
-                        break;
-                    }
-                }
+        for (int balance : inputArray4) {
+            if (balance < 0) {
+                outputArray4[0] = false;
+                break;
+            }
+        }
 
-                System.out.println("inputArray4 = " + java.util.Arrays.toString(inputArray4));
-                System.out.println("outputArray4 = " + outputArray4);
+        System.out.println("inputArray4 = " + java.util.Arrays.toString(inputArray4));
+        System.out.println("outputArray4 = " + java.util.Arrays.toString(outputArray4));
+
 
 //task 5
 
